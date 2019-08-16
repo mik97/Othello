eaf134937c7dfe475eadf994f0bf6335c9e5a0f6
Alessandra Boccuto <aless97b@gmail.com>
2019-08-17 01:05:22 +0200

    modified board

--- board.lua
+++ board.lua
@@ -1,32 +1,59 @@
 -- nil empty, 0 white, 1 black
-
--- if the two indices are integers, you can multiply the first one by a constant and then add the second index.
---constant = column number and row number
-constant = 8;
-
 board = {}
---initialize board with 4 central pieces
+
+--initialize board (all boards)
+function board.initialize(origin_x, origin_y, dimension, columns)
+  --square
+  x = origin_x;
+  y = origin_y;
+  dim = dimension
+  col = columns
+  
+  --circle (it represent the piece)
+  circle_x = x + dimension/2
+  circle_y = y + dimension/2
+  circleDim = dim/2.5
+end
+
+--create board with 4 central pieces
 --N B
 --B N
-
 function board.create(self)
-  self[4 * constant + 4] = 1
-  self[5 * constant + 5] = 1
-  self[4 * constant + 5] = 0
-  self[5 * constant + 4] = 0
+  for i=1,dim do
+    self[i] = {}
+  end
+    self[4][4] = 1
+    self[5][5] = 1
+    self[4][5] = 0
+    self[5][4] = 0
+    return self
 end
 
 --draw the board
-function board.draw(self, x, or_y, dim)
-  local y = or_y;
+function board.draw(self)
+  local loc_x = x;
+  local loc_y = y;
+
     for i=1, 8 do
       for j=1, 8 do
-        love.graphics.rectangle('line', x, y, dim, dim)  
-        y = y + dim
+        love.graphics.rectangle('line', loc_x, loc_y, dim, dim)  
+        loc_y = loc_y + dim
       end
-        x = x + dim
-        y = or_y
+        loc_x = loc_x + dim
+        loc_y = y
     end
 end
 
+--add pieces to the board
+function board.fill(self)
+  for i=1,dim do
+    for j=1,dim do
+      if self[i][j] == 0 then
+          love.graphics.circle('fill', circle_x + dim*(i-1), circle_y + dim*(j-1), circleDim)
+      elseif self[i][j] == 1 then
+          love.graphics.circle('line', circle_x + dim*(i-1), circle_y + dim*(j-1), circleDim)
+      end
+    end
+  end
+end
   
--- main.lua
+++ main.lua
@@ -1,9 +1,13 @@
 require("board")
 
-function love.load(s)
+function love.load()
+  love.window.maximize()
   origin_x = 50;
   origin_y = 50;
-  dim = 50;
+  dim = 70;
+  -- x, y, square dimension, number of columns
+  board.initialize(origin_x, origin_y, dim, 8)
+  board = board:create()
 end
 
 function love.update()
@@ -11,8 +15,6 @@ function love.update()
 end
 
 function love.draw()
-  board:draw(origin_x, origin_y, dim)
+  board:draw()
+  board:fill()
 end
-
-
-board:create()
