9417d79d0cf3c519de4145a18697c9db2d791973
Michele <michele.celozzi97@gmail.com>
2019-08-20 18:30:46 +0200

    Updated main and board, deleted tree and minimax

--- board.lua
+++ board.lua
@@ -1,11 +1,28 @@
--- nil empty, 0 white, 1 black
+-- nil empty,  0 black, 1 white
+
+--matrix[col][row]
 board = {}
+
+--direction table
+directions = {
+  up = {0, -1},
+  down = {0 , 1},
+  right = {1 , 0},
+  left = {-1, 0 },
+  rup = {1, -1},
+  lup = {-1, -1},
+  rdown = {1, 1},
+  ldown = {-1, 1}
+}
+
+--candidate squares
+candidates = {}
 
 --initialize board (all boards)
 function board.initialize(origin_x, origin_y, dimension, columns)
   --square
-  x = origin_x;
-  y = origin_y;
+  x = origin_x
+  y = origin_y
   dim = dimension
   col = columns
   
@@ -15,17 +32,18 @@ function board.initialize(origin_x, origin_y, dimension, columns)
   circleDim = dim/2.5
 end
 
---create board with 4 central pieces
+
+--create board with 4 central pieces 
 --N B
 --B N
 function board.create(self)
   for i=1,dim do
-    self[i] = {}
+    self[i] = {} --rows
   end
-    self[4][4] = 1
-    self[5][5] = 1
-    self[4][5] = 0
-    self[5][4] = 0
+    self[4][4] = 0
+    self[5][5] = 0
+    self[5][4] = 1
+    self[4][5] = 1
     return self
 end
 
@@ -44,16 +62,114 @@ function board.draw(self)
     end
 end
 
---add pieces to the board
+--draw pieces to the board
 function board.fill(self)
-  for i=1,dim do
-    for j=1,dim do
-      if self[i][j] == 0 then
-          love.graphics.circle('fill', circle_x + dim*(i-1), circle_y + dim*(j-1), circleDim)
-      elseif self[i][j] == 1 then
-          love.graphics.circle('line', circle_x + dim*(i-1), circle_y + dim*(j-1), circleDim)
+  for i=1, col do --columns (x)
+    for j= 1, col do --rows (y)
+      if self[i][j] == 0 then --black
+        love.graphics.circle('line', circle_x + dim*(i-1), circle_y + dim*(j-1), circleDim)
+      elseif self[i][j] == 1 then --white
+        love.graphics.circle('fill', circle_x + dim*(i-1), circle_y + dim*(j-1), circleDim)
       end
     end
   end
 end
+
+-- candidate square searching
+function board.searchSquares(self, color)
+  candidates = {} --vuoto
   
+  for i=1, col do --cols (x)
+    for j= 1, col do --rows (y)
+      if self[i][j] == color then
+        for k, v in pairs(directions) do
+          board:searchForDirection(i, j, color, k)
+        end
+      end
+    end
+  end
+end
+
+-- candidate square search (direction based)
+function board.searchForDirection(self, cell_x, cell_y, color, dir)
+  local oppositeColor = (color + 1) % 2
+  local isValid = false
+
+  cell_x = cell_x + directions[dir][1] 
+  cell_y = cell_y + directions[dir][2]
+  if cell_x > 8 or cell_x < 1 or cell_y > 8 or cell_y < 1 then return false end
+  
+  while self[cell_x][cell_y] == oppositeColor do
+  isValid = true
+  
+  cell_x = cell_x + directions[dir][1] 
+  cell_y = cell_y + directions[dir][2]
+
+  
+  -- check the next square in inside the game board
+    if cell_x > 8 or cell_x < 1 or cell_y > 8 or cell_y <1 then 
+      return false
+    end
+  end
+  
+  if isValid == true then
+      if self[cell_x][cell_y] == nil and cell_y <= 8 and cell_x <=8 then
+            table.insert(candidates, {cell_x, cell_y})
+      end
+    end
+end
+
+-- draw candidate squares on the board
+function board:drawCandidates(self, color)
+  --black: red color, white: green color
+  if color == 0 then love.graphics.setColor(1,0,0)
+  else love.graphics.setColor(0,1,0) end
+  for _, v in ipairs(candidates) do
+   love.graphics.rectangle('line', x + 10 + dim*(v[1]-1), y + 10 + dim*(v[2]-1), dim -20, dim-20)  
+  end
+  love.graphics.setColor(1,1,1) --set color to white (default)
+end
+
+
+--coordinates are {x, y} aka {columns, row}
+function board.addPiece(self, coor, color)
+  self[coor[1]][coor[2]] = color
+  board:revertPieces(coor, color)
+end
+
+
+function board.isCandidate(self, coor)
+  for k, v in ipairs(candidates) do
+    if (v[1] == coor[1] and v[2] == coor[2]) then return true end
+  end
+  return false
+end
+
+--coor = coordinata square dove si vuole inserire il pezzo
+function board.revertPieces (self, coor, color)
+  for k, v in pairs(directions) do
+    board:revertForDirection(coor[1], coor[2], color, v)
+  end
+end
+
+function board.revertForDirection(self, cell_x, cell_y, color, dir)
+  local oppositeColor = (color + 1) % 2
+  local toRevert = {}
+  cell_x = cell_x + dir[1]
+  cell_y = cell_y + dir[2] 
+
+  if cell_x > 8 or cell_x < 1 or cell_y > 8 or cell_y < 1 then return false end
+
+  while self[cell_x][cell_y] == oppositeColor do
+    table.insert(toRevert, {cell_x, cell_y})
+    -- calculate position next square
+    cell_x = cell_x + dir[1]
+    cell_y = cell_y + dir[2]
+    if cell_x > 8 or cell_x < 1 or cell_y > 8 or cell_y < 1 then return false end
+  end
+  if self[cell_x][cell_y] == color then
+    for _ , k in ipairs(toRevert) do
+      self[k[1]][k[2]] = color
+    end
+  end
+end
--- main.lua
+++ main.lua
@@ -2,12 +2,18 @@ require("board")
 
 function love.load()
   love.window.maximize()
+  current_player = 0
   origin_x = 50;
   origin_y = 50;
   dim = 70;
-  -- x, y, square dimension, number of columns
+  
+  -- selected = {x, y}: x is the column and y the row
+  selected = {2,4}
+  
   board.initialize(origin_x, origin_y, dim, 8)
   board = board:create()
+  
+  board:searchSquares(current_player)
 end
 
 function love.update()
@@ -17,4 +23,41 @@ end
 function love.draw()
   board:draw()
   board:fill()
+  board:drawCandidates()
+  drawSelected()
+end
+
+function love.keypressed(key, scancode, isrepeat)
+   
+  if key == "left" then
+    selected[1] = selected[1] - 1
+  end
+  
+  if key == "right" then
+    selected[1] = selected[1] + 1
+    
+  end
+  if key == "down" then
+    selected[2] = selected[2] + 1 
+  end
+  
+  if key == "up" then
+    selected[2] = selected[2] - 1
+  end
+ 
+  
+  if key == "return" then
+    if board:isCandidate(selected) then
+      board:addPiece(selected, current_player)
+      current_player = (current_player + 1) % 2
+      board:searchSquares(current_player)
+     end
+  end
+  
+end
+
+function drawSelected()
+  love.graphics.setColor(0,1,1)
+  love.graphics.rectangle('line', origin_x + 20 + dim*(selected[1]-1), origin_y + 20 + dim*(selected[2]-1), dim - 40, dim - 40)  
+  love.graphics.setColor(1,1,1) --set color to white (default)
 end
